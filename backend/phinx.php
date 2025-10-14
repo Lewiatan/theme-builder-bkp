aaf823b6164e13fe93dcf05053e8df3af66bb7ea
Maciej Niepieklo <mniepieklo@bettercollective.com>
2025-10-14 20:38:37 +0200

    add phinx config

---
+++ backend/phinx.php
@@ -0,0 +1,41 @@
+<?php
+
+return
+[
+    'paths' => [
+        'migrations' => '%%PHINX_CONFIG_DIR%%/db/migrations',
+        'seeds' => '%%PHINX_CONFIG_DIR%%/db/seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'pgsql',
+            'host' => 'postgres',
+            'name' => 'builder',
+            'user' => 'builder',
+            'pass' => 'builder',
+            'port' => '5432',
+            'charset' => 'utf8',
+        ],
+        'production' => [
+            'adapter' => 'psql',
+            'host' => 'localhost',
+            'name' => 'production_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => 'psql',
+            'host' => 'localhost',
+            'name' => 'testing_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
