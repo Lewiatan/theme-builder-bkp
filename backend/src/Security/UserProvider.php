cc4a9f1b99025484deede8eb488296f8da0a1bef
Maciej Niepieklo <mniepieklo@bettercollective.com>
2025-10-30 11:58:15 +0100

    fix issue with providing the valid user object

---
+++ backend/src/Security/UserProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use App\Model\Entity\User;
+use App\Model\ValueObject\Email;
+use App\Repository\UserRepository;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+
+final class UserProvider implements UserProviderInterface
+{
+    public function __construct(
+        private readonly UserRepository $userRepository
+    ) {}
+
+    public function loadUserByIdentifier(string $identifier): UserInterface
+    {
+        $email = new Email($identifier);
+        $user = $this->userRepository->findByEmail($email);
+
+        if ($user === null) {
+            throw new UserNotFoundException(sprintf('User with email "%s" not found.', $identifier));
+        }
+
+        return $user;
+    }
+
+    public function refreshUser(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof User) {
+            throw new \InvalidArgumentException(sprintf('Instances of "%s" are not supported.', get_class($user)));
+        }
+
+        return $this->loadUserByIdentifier($user->getUserIdentifier());
+    }
+
+    public function supportsClass(string $class): bool
+    {
+        return User::class === $class || is_subclass_of($class, User::class);
+    }
+}
